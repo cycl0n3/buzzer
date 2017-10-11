4f2d9d25f8a8da8b2c5e467ed4fdf46d91658a3d
Milind <7milind7@gmail.com>
2017-10-11 18:16:53 +0530

    some uploading

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -11,7 +11,8 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    # "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "#{Rails.root}/public/#{model.uuid}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
@@ -21,6 +22,10 @@ class AvatarUploader < CarrierWave::Uploader::Base
   #
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   # end
+
+  def default_url(*args)
+    '/profile.png'
+  end
 
   # Process files as they are uploaded:
   # process scale: [200, 300]
