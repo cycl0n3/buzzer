2fbfdcb4afb20658ac98721449959cc5ed0c0827
Milind <7milind7@gmail.com>
2017-10-11 17:20:48 +0530

    some uploading

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,13 @@
 class User < ApplicationRecord
   enum role: [:user, :vip, :admin]
+
   after_initialize :set_default_role, :if => :new_record?
+
+  mount_uploader :avatar, AvatarUploader
 
   def set_default_role
     self.role ||= :user
+    self.uuid ||= SecureRandom.uuid
   end
 
   # Include default devise modules. Others available are:
--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,9 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+require 'carrierwave'
+require 'carrierwave/orm/activerecord'
+
 module Buzzer
   class Application < Rails::Application
 
