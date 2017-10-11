a486330a1d626b26975588ab09f10f3aacecf34f
Milind <7milind7@gmail.com>
2017-10-11 15:12:25 +0530

    some uploading

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171008105107) do
+ActiveRecord::Schema.define(version: 20171011094113) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20171008105107) do
     t.datetime "updated_at", null: false
     t.string "name"
     t.integer "role"
+    t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
